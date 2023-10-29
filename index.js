967c08efb05e40fee6dee4d3411292d990365534
diegoram23 <diego.pta.oc@gmail.com>
2023-10-28 17:20:31 -0700

    JS function to refresh page after submitting form

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const submitBtn = document.getElementById('btn')
 const form = document.getElementById('form')
+const match = document.getElementById('match')
+const noMatch = document.getElementById('no-match')
 
 form.addEventListener('submit', (e) => {
     const email = document.getElementById('email').value
     const confEmail = document.getElementById('conf-email').value    
 
     if (email === confEmail) {
-        console.log('match')
+        match.style.display = 'block'
         e.preventDefault()
+        setTimeout(() => {
+            location.reload()
+        }, 1500);
     } else {
         e.preventDefault()
-        console.log('no match')
+        noMatch.style.display = 'block'
+        setTimeout(() => {
+            location.reload()
+        }, 1500);
     }
-
- 
 })
